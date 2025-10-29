222eb0e505f43a52e70b39e4c60ebba30e83dd03
Aliakbargh85 <131409388+Aliakbargh85@users.noreply.github.com>
2025-10-29 23:34:30 +0330

    Add files via upload

---
+++ Event ClipBoard/js/script.js
@@ -0,0 +1,20 @@
+﻿let editorElem = document.getElementById("editor");
+
+function copyHandler(e) {
+  e.preventDefault();
+  alert("Copy");
+}
+
+function cutHandler(e) {
+  e.preventDefault();
+  alert("Cut");
+}
+
+function pasteHandler(e) {
+  e.preventDefault();
+  alert("Paste");
+}
+
+editorElem.addEventListener("copy", copyHandler);
+editorElem.addEventListener("cut", cutHandler);
+editorElem.addEventListener("paste", pasteHandler);
